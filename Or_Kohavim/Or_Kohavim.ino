b05590a7ae616b51a6140a22eedc7f3a6c58da9b
FailedSuccessfully <itay.fichman@gmail.com>
2023-07-18 17:30:55 +0300

    rollback adhoc fix try

--- Or_Kohavim/Or_Kohavim.ino
+++ Or_Kohavim/Or_Kohavim.ino
@@ -6,21 +6,12 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(readPin, INPUT);
   readOut = 0;
-  highest = 0;
-  lowest = 1;
   Serial.begin(9600);
 
 }
 
 void loop() {
   readOut = analogRead(readPin);
-  if (readOut > highest){
-    highest = readOut;
-  }
-  if (readOut < lowest){
-    lowest = readOut;
-  }
-  int value = map(readOut, lowest, highest, 0, 1);
-  Serial.println(value * rate);
+  Serial.println(readOut * rate);
   delay(100);
 }
